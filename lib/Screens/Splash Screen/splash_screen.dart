8b9595e430813e80af07af515a5e6cbd5ee24461
hamzakhan2317 <hamzakhan2317@gmail.com>
2022-01-11 17:45:10 +0500

    Splash screens Done...

---
+++ lib/Screens/Splash Screen/splash_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '../../constant.dart';
+import 'on_board.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future<void> init() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    defaultBlurRadius = 10.0;
+    defaultSpreadRadius = 0.5;
+
+    finish(context);
+    const OnBoard().launch(context, isNewTask: true);
+  }
+
+  @override
+  void setState(fn) {
+    if (mounted) super.setState(fn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: kMainColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 3,
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: 100,
+                child: const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image(
+                    image: AssetImage('images/logo.png'),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  'Version 1.0.0',
+                  style: GoogleFonts.manrope(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
